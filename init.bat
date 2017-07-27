38bbf959fa6e47cb0825daa259078b8200811595
djohnson001 <djohnson001@gmail.com>
2017-07-26 21:34:21 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,3 +1,13 @@
 REM Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+cinst boxstarter -y
+
+BoxStarterShell
+
+Set-TaskbarOptions -Combine Never -Size Small
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -DisableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -DisableOpenFileExplorerToQuickAccess -DisableShowRecentFilesInQuickAccess -DisableShowFrequentFoldersInQuickAccess 
+
+Disable-InternetExplorerESC
+Disable-GameBarTips
+Disable-BingSearch
